7ad0a5d461b3f4a0a21de77bdf72ed9275226dea
Carl Tayo <tayocarl@gmail.com>
2017-08-30 13:59:44 +0300

    changes to my js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,42 @@
-function pingPongClick() {
-  var number = document.getElementById("txt_pp").value;
-  var result = "";
-  for (var i = 1; i <= number; i++) {
-    if (i % 15 == 0) {
-      result += "<li>PingPong</li>";
-    } else if (i % 5 == 0) {
-      result += "<li>Pong</li>";
-    } else if (i % 3 == 0) {
-      result += "<li>Ping</li>";
-    } else {
-      result += "<li>" + i + "</li>";
-    }
+//Back-end logic
 
-  }
-  document.getElementById("ans").innerHTML = result;
+var pingpongFilter = function(numberEntered){
+  var outPuts =[];
+  for(var index = 1; index <= numberEntered; index += 1){
+    if (index % 3 === 0 && index % 5 === 0) {
+      outPuts.push("pingpong")
+    } else if (index % 3 === 0) {
+      outPuts.push("ping")
+    } else if (index % 5 === 0) {
+      outPuts.push("pong")
+    } else {
+      outPuts.push(index)
+    }
+  };
+return outPuts;
+};
+
+var clearPrevious = function(){
+  $('#number').val("");
+  $('ul').empty();
+}
+
+
+//Front-end logic
+
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+    //Get value of numberEntered
+    var numberEntered = parseInt($("#number").val());
+
+    //Clear input field and ul#output before append
+    clearPrevious();
+
+    //Call the function pingpongFilter and append each element of its resulting array to ul with id="output" tag
+    pingpongFilter(numberEntered).forEach(function(number) {
+      $('#output').append('<li>'+number+'</li>')
+    })
+  });
+});
